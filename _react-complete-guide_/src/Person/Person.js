ab5a6eaec1b9ce8c1ad5afed76c25bbd67d3d333
Aga <a.kolia.grudzien@gmail.com>
2018-05-11 20:55:56 +0200

    children property

--- _react-complete-guide_/src/Person/Person.js
+++ _react-complete-guide_/src/Person/Person.js
@@ -4,7 +4,15 @@ import React from 'react';
 
 //props allows us to manage properties of the component
 const  person = (props) => {
-  return <p>I am {props.name} and I am {props.age}!</p>
+  return (
+    <div>
+      <p>I am {props.name} and I am {props.age}!</p>
+      <p>{props.children}</p>
+    </div>
+  )
 };
 
 export default person;
+
+//children props refers to every element that goes between opening and closing
+//tag of our component - in this case hobbies
