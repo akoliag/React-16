6c3a0f42375eddedf681c1d81a87107f55a7cb87
Aga <a.kolia.grudzien@gmail.com>
2018-05-11 19:48:43 +0200

    create functional component

---
+++ _react-complete-guide_/src/Person/Person.js
@@ -0,0 +1,9 @@
+//creating a functional (stateless) Component with ES6 arrow function
+
+import React from 'react';
+
+const  person = () => {
+  return <p>Im a Person!</p>
+};
+
+export default person;
