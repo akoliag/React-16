e50c7149268c6656569b52c3278418d561df26fb
Aga <a.kolia.grudzien@gmail.com>
2018-05-11 21:39:36 +0200

    add state

--- _react-complete-guide_/src/App.js
+++ _react-complete-guide_/src/App.js
@@ -3,17 +3,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Aga', age: 30},
+      {name: 'Steve', age: 21}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>This is a React App</h1>
         <p>It is really working!</p>
-        <Person name="Max" age="28"/>
-        <Person name="Aga" age="30"/>
-        <Person name="Steve" age="21">My Hobbies: Racing and Sailing</Person>
-        </div>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[2].age}>My Hobbies: Racing and Sailing</Person>
+      </div>
     );
   }
 }
 
 export default App;
+//state is available only class extending Component, it's not available
+//in function component
