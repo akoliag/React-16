11e37dc212505cc8a183de1083e74cbded9fc959
Aga <a.kolia.grudzien@gmail.com>
2018-05-11 20:26:13 +0200

    output dynamic content

--- _react-complete-guide_/src/Person/Person.js
+++ _react-complete-guide_/src/Person/Person.js
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const  person = () => {
-  return <p>Im a Person!</p>
+  return <p>I am a Person and I am {Math.floor(Math.random()*30)}!</p>
 };
 
 export default person;
