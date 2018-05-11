f05e0f6aeb567395b34b61cbfd4e7c29d5273d03
Aga <a.kolia.grudzien@gmail.com>
2018-05-11 19:50:14 +0200

    add person

--- _react-complete-guide_/src/App.js
+++ _react-complete-guide_/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, Im a React App</h1>
+        <h1>This is a React App</h1>
+        <p>It is really working!</p>
+        <Person />
         </div>
     );
   }
