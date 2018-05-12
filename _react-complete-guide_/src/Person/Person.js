53313dbcf32533b58357ce2eb05d2aa6adda5f9e
Aga <a.kolia.grudzien@gmail.com>
2018-05-12 19:02:21 +0200

    add styling to Person

--- _react-complete-guide_/src/Person/Person.js
+++ _react-complete-guide_/src/Person/Person.js
@@ -1,11 +1,12 @@
 //creating a functional (stateless) Component with ES6 arrow function
 
 import React from 'react';
+import './Person.css';
 
 //props allows us to manage properties of the component
 const  person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>I am {props.name} and I am {props.age}!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
