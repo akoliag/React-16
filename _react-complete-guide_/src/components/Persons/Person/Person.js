75688d9e3876d2b72d008b86ba966f7bf9c92f34
Aga <a.kolia.grudzien@gmail.com>
2018-05-29 21:18:04 +0200

    convert stateless to stateful components

--- _react-complete-guide_/src/components/Persons/Person/Person.js
+++ _react-complete-guide_/src/components/Persons/Person/Person.js
@@ -1,22 +1,23 @@
 //creating a functional (stateless) Component with ES6 arrow function
 
-import React from 'react';
+import React, {Component} from 'react';
 import classes from'./Person.css';
 
-
-const  person = (props) => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I am {props.name} and I am {props.age}!</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-  )
+class Person extends Component {
+  render () {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age}!</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      </div>
+    )
+  }
 };
 //props allows us to manage properties of the component
 //we can pass methods also as props so we can call the method that changes the state
 //in another component which does not have a direct access to the state
-export default person;
+export default Person;
 
 //children props refers to every element that goes between opening and closing
 //tag of our component - in this case hobbies
