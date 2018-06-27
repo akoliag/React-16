3fbb13caca38c4d4e4acdb1908611aaae3c1f7e3
Aga <a.kolia.grudzien@gmail.com>
2018-06-27 20:02:04 +0200

    create a http request to GET data

--- PostApp_/src/containers/Blog/Blog.js
+++ PostApp_/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,6 +7,14 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+  componentDidMount () {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log(response);
+      });
+  }
+
     render () {
         return (
             <div>
@@ -26,3 +35,6 @@ class Blog extends Component {
 }
 
 export default Blog;
+//then is a method that takes a function as the input and this function will get
+//executed once the data from the backend is there. It receives 'response' object
+//as input and this object is automatically passed by axios
